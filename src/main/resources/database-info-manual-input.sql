c56ad1da6fd38a18764b536895ebc94eab3729c2
VBakaryan <vahan.bakaryan@gmail.com>
2017-06-13 15:44:27 +0400

    shop.jsp and shop-detail.jsp pages are modified.

--- src/main/resources/database-info-manual-input.sql
+++ src/main/resources/database-info-manual-input.sql
@@ -35,12 +35,12 @@ INSERT INTO artist(user_id,spec_id,photo) VALUES(3,4,12321321);
 INSERT INTO artist(user_id,spec_id,photo) VALUES(5,3,12321321);
 
 
-INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 1','Test Desc 1','../../resources/images/product/art-deco-600x600.jpg',100,1,0,'SCULPTURE');
-INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 2','Test Desc 2','../../resources/images/product/art-deco-600x600.jpg',250,1,0,'PHOTOGRAPHY');
-INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 3','Test Desc 3','../../resources/images/product/art-deco-600x600.jpg',450,1,0,'PAINTING');
-INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 4','Test Desc 4','../../resources/images/product/art-deco-600x600.jpg',50,1,0,'OTHER');
-INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 5','Test Desc 5','../../resources/images/product/art-deco-600x600.jpg',180,2,0,'PHOTOGRAPHY');
-INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 6','Test Desc 6','../../resources/images/product/art-deco-600x600.jpg',1000,2,0,'SCULPTURE');
-INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 7','Test Desc 7','../../resources/images/product/art-deco-600x600.jpg',300,3,0,'PAINTING');
-INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 8','Test Desc 8','../../resources/images/product/art-deco-600x600.jpg',560,5,0,'OTHER');
+INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 1','Test Desc 1','../../resources/images/product/images (1).jpg',100,1,0,'SCULPTURE');
+INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 2','Test Desc 2','../../resources/images/product/images (2).jpg',250,1,0,'PHOTOGRAPHY');
+INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 3','Test Desc 3','../../resources/images/product/images (3).jpg',450,1,0,'PAINTING');
+INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 4','Test Desc 4','../../resources/images/product/images (4).jpg',50,1,0,'OTHER');
+INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 5','Test Desc 5','../../resources/images/product/images (5).jpg',180,2,0,'PHOTOGRAPHY');
+INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 6','Test Desc 6','../../resources/images/product/images (6).jpg',1000,2,0,'SCULPTURE');
+INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 7','Test Desc 7','../../resources/images/product/images (7).jpg',300,3,0,'PAINTING');
+INSERT INTO item(title,description,photo_url,price,artist_id,status,type) VALUES('Title 8','Test Desc 8','../../resources/images/product/images (8).jpg',560,5,0,'OTHER');
 
