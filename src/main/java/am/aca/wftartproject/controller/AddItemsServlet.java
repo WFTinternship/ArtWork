330b3764dbef6472181922f6e5da6588f6954233
Armen <manukyanarm@yahoo.com>
2017-06-17 12:14:54 +0400

    additem, my ArtWorks pages were added, purchase history page was modified, getArtistItems func was modified in DAO layer

---
+++ src/main/java/am/aca/wftartproject/controller/AddItemsServlet.java
@@ -0,0 +1,7 @@
+package am.aca.wftartproject.controller;
+
+/**
+ * Created by Armen on 6/16/2017.
+ */
+public class AddItemsServlet {
+}
---
+++ src/main/java/am/aca/wftartproject/controller/MyWorksServlet.java
@@ -0,0 +1,10 @@
+package am.aca.wftartproject.controller;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by Armen on 6/16/2017.
+ */
+public class MyWorksServlet extends HttpServlet {
+
+}
