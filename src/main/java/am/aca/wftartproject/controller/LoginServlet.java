eaa8e05cd574ba96491038943c7e64e193da58cc
tatev <shtatev4991@gmail.com>
2017-06-20 14:37:12 +0400

    Sign up and Log in pages are modified.

--- src/main/java/am/aca/wftartproject/controller/LoginServlet.java
+++ src/main/java/am/aca/wftartproject/controller/LoginServlet.java
@@ -15,7 +15,7 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/logInSignUp.html");
+        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/logIn.html");
         dispatcher.forward(request, response);
     }
 
